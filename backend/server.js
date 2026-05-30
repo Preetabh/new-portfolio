e3c4de2d741cd61597e41d0ec74747b2cc725806
vishubbk <vishubbkup@gmail.com>
2026-05-30 11:20:04 +0530

    cors error solved

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "http://127.0.0.1:3000",
+      "https://preetabh-portfolio.onrender.com/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
