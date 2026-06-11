2211e3efdbebc4e4a80b8c2ffecf8c3fd57b9736
vishubbk <vishubbkup@gmail.com>
2026-06-11 11:34:07 +0530

    core problem resolve

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://preetabh-portfolio.onrender.com/",
+      "https://preetabh-portfolio.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
