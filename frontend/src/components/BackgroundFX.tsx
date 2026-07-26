ec0ef6d024aa226bf2d5fd4ade58c68db6bb7147
vishubbk <vishubbkup@gmail.com>
2026-07-27 00:37:21 +0530

    .

--- frontend/src/components/BackgroundFX.tsx
+++ frontend/src/components/BackgroundFX.tsx
@@ -1,5 +1,5 @@
-import Ballpit from "./Ballpit";
+import PrismaticBurst from "./PrismaticBurst";
 
 export default function BackgroundFX() {
-  return <Ballpit count={38} minSize={14} maxSize={30} gravity={0.015} />;
+  return <PrismaticBurst raysCount={32} speed={0.002} intensity={0.75} interactive={true} />;
 }
